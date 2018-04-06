6b735caaf6e8170940da4bc5800cc2a5add238cd
Meggan M Green <github@meggangreen.com>
2018-04-05 18:58:10 -0700

    finished age and relationship validation

--- index.js
+++ index.js
@@ -9,47 +9,68 @@
 // After submission the user should be able to make changes and submit the
 // household again.
 
-use strict;
+"use strict";
 
 // onClick listeners: submit, add, edit-id, remove-id
 // validation: age > 0, relationship !== '---'
 //      'add' button disabled until validation successful
 
+
 // Variable assignments
 let addButton = document.getElementsByTagName("button")[0];
 let submitButton = document.getElementsByTagName("button")[1];
 let ageField = document.getElementsByName("age")[0];
-let relationField = document.getElementsByName("relationship")[0];
+let relationField = document.getElementsByName("rel")[0];
 let smokerField = document.getElementsByName("smoker")[0];
 
+// First thing on page load
+addButton.disabled = true;
+submitButton.disabled = true;
 
-// Validation listeners
-ageField.onkeyup(function() {
-    let ageInput = Number(ageField.value);
-    if ( ( isNaN(ageInput) ) || ( ageInput <= 0 ) ) {
+// Add event listeners
+if (ageField.addEventListener) {
+    ageField.addEventListener("keyup", validateInputs);
+} else if (ageField.attachEvent) {  // IE8 and earlier
+    ageField.attachEvent("onkeyup", validateInputs);
+} // end if -- ageField keyup
 
-    }
-});
-
-
-function add_person(age, relationship, smoker=null) {
-    // add a list item to the hhlist:
-    //      id, age, relationship, 'non' | 'smoker', edit, remove
+if (relationField.addEventListener) {
+    relationField.addEventListener("change", validateInputs);
+} else if (relationField.attachEvent) {  // IE8 and earlier
+    relationField.attachEvent("onchange", validateInputs);
 }
 
 
-function edit_person(id) {
+// Validation functions
+function validateInputs() {
+    let ageInput = Number(ageField.value);
+    let relationInput = relationField.value;
+    if ( (isNaN(ageInput)) || (ageInput <= 0) || (relationInput === '') ) {
+        addButton.disabled = true;
+    } else {
+        addButton.disabled = false;
+    } // end if
+} // end validateInputs
+
+
+function addPerson(age, relationship, smoker=null) {
+    // add a list item to the hhlist:
+    //      id, age, relationship, 'non' | 'smoker', edit, remove
+} // end addPerson
+
+
+function editPerson(id) {
     // calls to remove list item from hhlist ?
     // places list item data in form ready to add (update)
 }
 
 
-function remove_person(id) {
+function removePerson(id) {
     // removes list item from hhlist
 }
 
 
-function submit_household() {
+function submitHousehold() {
     // compile payload
     // send ajax call to update db
     //      put json into 'debug'

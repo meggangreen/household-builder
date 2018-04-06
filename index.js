8676f13f1f00ef3e5ba8ad5843c14a6604bde66b
Meggan M Green <github@meggangreen.com>
2018-04-05 17:10:10 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// To Do:
+// Validate data entry (age is required and > 0, relationship is required)
+// Add people to a growing household list
+// Remove a previously added person from the list
+// Display the household list in the HTML as it is modified
+// Serialize the household as JSON upon submission as a fake trip to the server
+// On submission, put the serialized JSON in the provided "debug" DOM element
+// and display that element.
+// After submission the user should be able to make changes and submit the
+// household again.
+
+function add_person(age, relationship, smoker=null) {
+    // add a list item to the hhlist
+    // age, relationship, 'non' | 'smoker', edit, remove
+}

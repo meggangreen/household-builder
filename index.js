2b49da5e91cdf277075c3c10105428a70af78511
Meggan M Green <github@meggangreen.com>
2018-04-05 19:00:06 -0700

    finished age and relationship validation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // To Do:
-// Validate data entry (age is required and > 0, relationship is required)
-// Add people to a growing household list
-// Remove a previously added person from the list
-// Display the household list in the HTML as it is modified
-// Serialize the household as JSON upon submission as a fake trip to the server
-// On submission, put the serialized JSON in the provided "debug" DOM element
-// and display that element.
-// After submission the user should be able to make changes and submit the
-// household again.
+// [x] Validate data entry (age is required and > 0, relationship is required)
+// [ ] Add people to a growing household list
+// [ ] Remove a previously added person from the list
+// [ ] Display the household list in the HTML as it is modified
+// [ ] Serialize the household as JSON upon submission as a fake trip to the server
+// [ ] On submission, put the serialized JSON in the provided "debug" DOM element
+// [ ] and display that element.
+// [ ] After submission the user should be able to make changes and submit the
+// [ ] household again.
 
 "use strict";
 
-// onClick listeners: submit, add, edit-id, remove-id
-// validation: age > 0, relationship !== '---'
+// [ ] onClick listeners: submit, add, edit-id, remove-id
+// [x] validation: age > 0, relationship !== ''
 //      'add' button disabled until validation successful
 
 
